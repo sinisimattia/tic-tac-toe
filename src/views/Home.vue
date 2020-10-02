eb1a3642b0013268d39c66c7e700c7740d4753ec
Mattia Sinisi <mattia@snisni.it>
2020-10-02 15:37:42 +0200

    Customized router setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<div class="hero is-fullheight">
+			<div class="hero-body container">
+				<div class="has-text-centered">
+					<h1 class="title is-1">
+						<span>Tic</span>
+						<span class="has-text-primary">Tac</span>
+						<span>Toe</span>
+					</h1>
+					<h2 class="subtitle is-3">but with emojis</h2>
+				</div>
+			</div>
+
+			<div class="hero-foot">
+				<div class="container">
+					<p class="has-text-centered has-text-grey">Mattia Sinisi</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+};
 </script>
