ad82f46721bee9dce57d942a4a84df9d56c36ff4
Mattia Sinisi <mattia@snisni.it>
2020-10-02 16:02:23 +0200

    Using hero banner

--- src/views/Prepare.vue
+++ src/views/Prepare.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <h1>Choose emojis and size of grid</h1>
+  <div class="hero is-fullheight is-info">
+    <div class="hero-body container">
+      <div class="box">
+        <h1>Choose emojis and size of grid</h1>
+      </div>
+    </div>
   </div>
 </template>
