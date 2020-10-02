2cd32c7305070bb7cfe4b7eb740bc513134a9a89
Mattia Sinisi <mattia@snisni.it>
2020-10-02 15:49:31 +0200

    Created double-sided view

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    <router-view/>
-  </div>
+	<div class="columns is-vcentered is-marginless is-paddingless">
+    <div class="column is-paddingless">
+      <div class="hero is-fullheight">
+			<div class="hero-body container">
+				<div class="has-text-centered">
+					<h1 class="title is-1">
+						<span>Tic</span>
+						<span class="has-text-primary">Tac</span>
+						<span>Toe</span>
+					</h1>
+					<h2 class="subtitle is-3">but with emojis</h2>
+				</div>
+			</div>
+
+			<div class="hero-foot">
+				<div class="container">
+					<p class="has-text-centered has-text-grey">Mattia Sinisi</p>
+				</div>
+			</div>
+		</div>
+    </div>
+		
+
+    <div class="column is-paddingless">
+      <router-view />
+    </div>
+	</div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,15 @@
 <template>
-	<div id="app">
-		<div class="hero is-fullheight">
-			<div class="hero-body container">
-				<div class="has-text-centered">
-					<h1 class="title is-1">
-						<span>Tic</span>
-						<span class="has-text-primary">Tac</span>
-						<span>Toe</span>
-					</h1>
-					<h2 class="subtitle is-3">but with emojis</h2>
-				</div>
-			</div>
-
-			<div class="hero-foot">
-				<div class="container">
-					<p class="has-text-centered has-text-grey">Mattia Sinisi</p>
-				</div>
+	<div class="hero is-fullheight is-dark">
+		<div class="hero-body container">
+			<div class="column">
+				<router-link
+					:to="{ name: 'Prepare' }"
+					tag="button"
+					class="button is-info is-rounded is-large"
+				>
+					Begin
+				</router-link>
 			</div>
 		</div>
 	</div>
 </template>
-
-<script>
-export default {
-	name: "App",
-};
-</script>
