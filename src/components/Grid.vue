59668d9a7410758ef1a429ecd0293eb59e3234d3
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-08 15:16:17 +0200

    Better styling

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,19 +1,29 @@
 <template>
-	<div class="box tile is-ancestor">
+	<div class="tile is-ancestor">
 		<div class="tile is-parent is-vertical" v-for="y in size" :key="y">
-      <!-- TODO Use dynamic slot names -->
-			<button class="tile is-child button is-large is-dark" v-for="x in size" :key="x">({{x}},{{y}})</button>
+			<!-- TODO Use dynamic slot names -->
+			<span
+				class="tile is-child box"
+				v-for="x in size"
+				:key="x"
+			>
+				<div class="is-square has-min-size">
+					<div class="content is-flex">
+            <div class="is-centered">({{ x }},{{ y }})</div>
+          </div>
+				</div>
+			</span>
 		</div>
 	</div>
 </template>
 
 <script>
 export default {
-  props: {
-    size: {
-      type: Number,
-      default: 3,
-    }
-  }
+	props: {
+		size: {
+			type: Number,
+			default: 3,
+		},
+	},
 };
 </script>
