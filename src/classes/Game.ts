872dac9c96a77f564cdd610c0f2657fcf43b1f84
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-08 17:19:28 +0200

    Better dictionary initialization

--- src/classes/Game.ts
+++ src/classes/Game.ts
@@ -1,6 +1,7 @@
 import { Dictionary } from '@/classes/Dictionary';
 import { Grid } from '@/classes/Grid';
 import { Representable } from '@/interfaces/Representable';
+import { Tile } from './Tile';
 
 export class Game implements Representable {
     private _grid: Grid;
@@ -8,13 +9,15 @@ export class Game implements Representable {
     private _turnIndex: number = 0;
     private _playersDictionary: Dictionary<string, number>;
 
-    constructor(gridSize: number = 3, symbols: string[] = ["", "X", "O"]){
+    constructor(gridSize: number = 3, symbols: string[] = ["X", "O"]){
         this._grid = new Grid(gridSize);
 
         this._playersDictionary = new Dictionary<string, number>();
 
+        this._playersDictionary.add(Tile.EMPTY, "")
+
         symbols.forEach((value, index) => {
-            this._playersDictionary.add(index, value)
+            this._playersDictionary.add(index + 1, value)
         })
     }
 
