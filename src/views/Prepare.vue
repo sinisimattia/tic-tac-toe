019e4aaf3b35028023365fab612a5959e931ca11
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-08 14:47:35 +0200

    Added button to begin

--- src/views/Prepare.vue
+++ src/views/Prepare.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="hero is-fullheight is-info">
     <div class="hero-body container">
-      <div class="box">
-        <h1>Choose emojis and size of grid</h1>
+      <div>
+        <div class="box">
+          <h1>Choose emojis and size of grid</h1>
+        </div>
+
+        <div>
+          <router-link class="button is-large is-rounded is-success" :to="{name: 'Play'}">Play</router-link>
+        </div>
       </div>
     </div>
   </div>
