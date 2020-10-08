580dcbd8e3d25c05a2df8e98ddb0435d2b245fe0
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-08 16:36:21 +0200

    Use index as slot name

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,12 +1,19 @@
 <template>
 	<div class="tile is-ancestor">
-		<div class="tile is-parent is-vertical" v-for="x in size" :key="x">
+		<div class="tile is-parent is-vertical" v-for="x in content.side" :key="x">
 			<!-- TODO Use dynamic slot names -->
-			<span class="tile is-child box is-clickable" v-for="y in size" :key="y" @click="select(x, y)">
+			<span
+				class="tile is-child box is-clickable"
+				v-for="y in content.side"
+				:key="y"
+				@click="select(x, y)"
+			>
 				<div class="is-square has-min-size">
 					<div class="content is-flex">
 						<div class="is-centered">
-							<slot :name="`tile-${(y - 1) + (size * (x - 1))}`">({{ x }},{{ y }})</slot>
+							<slot :name="`tile-${y - 1 + content.side * (x - 1)}`"
+								>({{ x }},{{ y }})</slot
+							>
 						</div>
 					</div>
 				</div>
@@ -18,15 +25,12 @@
 <script>
 export default {
 	props: {
-		size: {
-			type: Number,
-			default: 3,
+		content: Object
+	},
+	methods: {
+		select(x, y) {
+			this.$emit("selected", { x: x, y: y });
 		},
-  },
-  methods: {
-    select(x, y){
-      this.$emit('selected', {x: x, y: y})
-    }
-  }
+	},
 };
 </script>
