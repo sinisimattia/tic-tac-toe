ebd4975acf5944c46b588eba526cee7ba3e00d62
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-09 12:53:53 +0200

    New emoji-styled homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
-	<div class="hero is-fullheight is-dark">
+	<div class="hero is-fullheight is-dark background-pattern" :style="`background-image: url(${pattern})`">
 		<div class="hero-body container">
 			<div class="column">
 				<router-link
 					:to="{ name: 'Prepare' }"
 					tag="button"
-					class="button is-info is-rounded is-large"
+					class="button is-rounded is-large cta"
 				>
 					Begin
 				</router-link>
@@ -13,3 +13,20 @@
 		</div>
 	</div>
 </template>
+
+<script>
+import pattern from "@/assets/img/pattern.svg"
+export default {
+	data() {
+		return {
+			pattern
+		}
+	}
+}
+</script>
+
+<style scoped>
+.cta {
+	box-shadow: 0 0 30px black;
+}
+</style>
