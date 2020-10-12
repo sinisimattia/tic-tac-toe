81a437e65e73c983b95a8adbe9b406eb377b98c9
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-12 18:02:16 +0200

    Installed Sweet Alert 2

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import App from '@/App.vue'
 import "@/assets/scss/app.scss"
 import router from '@/router'
 import store from '@/store'
+import Swal from 'vue-sweetalert2';
 
 Vue.config.productionTip = false
+
+Vue.use(Swal)
 
 new Vue({
   router,
