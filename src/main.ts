cc7e4b8367e36ccfd72b614df2e53ba83a453245
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-10-13 10:41:10 +0200

    Custom styling for popups

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,17 @@ import Swal from 'vue-sweetalert2';
 
 Vue.config.productionTip = false
 
-Vue.use(Swal)
+Vue.use(Swal, {
+  buttonsStyling: false,
+  customClass: {
+    container: 'container-class',
+    popup: 'notification is-rounded has-shadow',
+    header: 'header-class',
+    title: 'title is-1',
+    content: 'subtitle is-4',
+    confirmButton: 'button is-medium is-dark is-rounded',
+  },
+})
 
 new Vue({
   router,
